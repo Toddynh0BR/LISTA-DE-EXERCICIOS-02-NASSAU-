494232a39ed419225a06330e6114d037ed4f3a5e
Toddynh0BR <galaxyplay41@gmail.com>
2026-04-01 17:16:17 -0300

    finalização da questão 9

--- Q10.py
+++ Q10.py
@@ -10,13 +10,13 @@
 #"Estoque baixo! Repor imediatamente."  
 #• Se o estoque chegar a 0, encerrar o programa automaticamente.
 
-nomeProduto = input("Digite o nome do produto: ")#pegar nome do produto
+nomeProduto = input("✏️ Digite o nome do produto: ")#pegar nome do produto
 
 #while para pegar estoque de produtos e já validar se é um numero ou não
 while True: 
- estoqueString = input(f'Digite o número em estoque de {nomeProduto}: ').strip()
+ estoqueString = input(f'🔄 Digite o número em estoque de {nomeProduto}: ').strip()
  if not estoqueString.isdigit():
-  print('Erro: Digite um valor válido.')
+  print('⁉️ Erro: Digite um valor válido.')
   continue
  
  estoque = int(estoqueString)
@@ -26,27 +26,27 @@ while True:
 while estoque > 0:#checar se estoque é menor que zero
  
  if estoque <= 10:#avisar caso estoque seja menor ou igual a 10
-   print("Estoque baixo! Repor imediatamente.")
+   print("❗Estoque baixo! Repor imediatamente.")
 
- print(f"Estoque de {nomeProduto}: {estoque}")
+ print(f"📊 Estoque de {nomeProduto}: {estoque}")
 
- opcao = input("Digite 1 para registrar uma venda ou 0 para alterar estoque: ")
+ opcao = input("❔ Digite 1 para registrar uma venda ou 0 para alterar estoque: ")
  #escolher oque usuario quer fazer
 
  if not opcao == '1' and not opcao == '0':
-  print('Erro: Digite uma opção válida.')
+  print('⁉️ Erro: Digite uma opção válida.')
   continue
 
  if opcao == '1':#caso queira registrar venda
-  quantidade = int(input("Digite a quantidade vendida: "))
+  quantidade = int(input("💲Digite a quantidade vendida: "))
   if quantidade > estoque:#evitar que estoque fique negativo
-   print("Erro: Quantidade insuficiente em estoque.")
+   print("⁉️ Erro: Quantidade insuficiente em estoque.")
    continue
   estoque -= quantidade#subtrair estoque
   
  else:#caso queira alterar numero de estoque
-  novoEstoque = int(input("Digite o novo estoque: "))
-  print(f'Novo estoque: {novoEstoque}')
+  novoEstoque = int(input("🔄 Digite o novo estoque: "))
+  print(f'✔️ Novo estoque: {novoEstoque}')
   estoque = novoEstoque
 
 print('Estoque no fim. 😭😭😭')
--- Q9.py
+++ Q9.py
@@ -13,28 +13,77 @@
 #• Percentual de clientes com idade acima de 50 anos 
 
 #declaração de variáveis
+repeticoes = 3
 salarios = 0
 maior_idade = 0
-menor_idade = 0
+menor_idade = None
 quantidade_feminino = 0
 quantidade_acima_50 = 0
 
 #loop para cadastro dos clientes
-for i in range(20):
-  if i >= 20:
+for i in range(repeticoes):
+  if i >= repeticoes:
     break
   else:
-    #while para pegar idade e validar
+    #while para pegar idade, validar e definir valores com base na idade
     while True:
      idade = input('Digite a idade do cliente: ')
      #if que verifica se é um número, caso contrário, pede para digitar novamente retornando para o inicio do while
      if not idade.isdigit():
       print('Digite um número válido!')
-      continue  
+      continue #retorna para o inicio do while se entrar nesse if
+     
+     #Define Maior Idade
+     if int(idade) > maior_idade: 
+      maior_idade = int(idade)
+  
+     #Define Menor Idade
+     if menor_idade is None or int(idade) < menor_idade:
+       menor_idade = int(idade)
       
+     #Define Maior que 50
+     if int(idade) > 50:
+       quantidade_acima_50 += 1
      break
     
-        
+    #while para pegar sexo...
+    while True:
+     sexo = input('Digite seu sexo(M/F): ')
+
+     #if para verificar se digitou um genero válido ou não
+     if sexo != 'M' and sexo != 'F':
+       print("Digite um sexo válido(M/F)!")
+       continue
+     
+     break
+   
+    #while para pegar salario
+    while True:
+     salario = input('Digite seu salário(ex: 1500): ').replace(',', '.', 1)
+
+     #checar numero digitado pelo usuario
+     if not salario.replace(".", "", 1).isdigit():
+       print("Digite um número válido!")
+       continue
+     
+     #checar se é mulher e ganha mais de 3000
+     if float(salario) > 3000 and sexo == 'F':
+       quantidade_feminino += 1
+     
+     #incrementar salarios para tirar a média depois
+     salarios += float(salario)
+     break
+ 
+    print('Cliente Cadastrado!\n')
+    
+#print com resultados
+print(f"""📊Resultado da análise dos {repeticoes} clientes:
+💸Média salarial: {(salarios/repeticoes):.2f};
+🧓Maior Idade: {maior_idade};
+🧒Menor Idade: {menor_idade};
+👩Mulheres com salario maior que R$ 3.000: {quantidade_feminino};
+🧔Clientes com idade acima de 50 anos: {quantidade_acima_50};   
+""")       
         
         
         
